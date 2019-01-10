c93bbb9dca8d9be9b51048ff2e6e055825f76337
zhongyu <931564975@qq.com>
2019-01-10 10:41:54 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+// import UniUI from 'uni-ui'
+import App from './App'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$serverUrl = 'https://unidemo.dcloud.net.cn';
+
+App.mpType = 'app'
+
+// Vue.use(UniUI)
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
